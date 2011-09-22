372763b540c57d3d1896c0f3f82ffc7726663a44
Justin M. LaPre <justin@lapre.com>
2011-09-22 12:03:07 -0400

    Added bit field manip #define's.

--- olsr.h
+++ olsr.h
@@ -11,6 +11,16 @@
  * - single interface
  */
 
+// From http://c-faq.com/misc/bitsets.html
+#include <limits.h>		/* for CHAR_BIT */
+
+#define BITMASK(b) (1 << ((b) % CHAR_BIT))
+#define BITSLOT(b) ((b) / CHAR_BIT)
+#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
+#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
+#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
+#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
+
 #include "ross.h"
 
 /** HELLO message interval */
